c6e624443df3935b8a019bb767f9135158e31917
Alex <alexamuek@gmail.com>
2023-05-25 10:37:07 +0800

    mock fix

--- contracts/mock/MinterServiceWithAgent.sol
+++ contracts/mock/MinterServiceWithAgent.sol
@@ -2,85 +2,15 @@
 pragma solidity 0.8.19;
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
-import '../ServiceProvider.sol';
+import '../ServiceProviderOwnable.sol';
 
-contract MinterServiceWithAgent is ERC721URIStorage, ServiceProvider, Ownable {
+contract MinterServiceWithAgent is ERC721URIStorage, ServiceProviderOwnable {
 	
 	constructor(address _subscrRegistry, string memory name_,
         string memory symbol_) 
 			ERC721(name_, symbol_)
-	        ServiceProvider(_subscrRegistry) {}
-
-	    function registerServiceTariff(Tariff memory _newTariff) 
-	        external onlyOwner returns(uint256)
-	    {
-	        return _registerServiceTariff(_newTariff);
-	    }
-
-	    function editServiceTariff(
-	        uint256 _tariffIndex, 
-	        uint256 _timelockPeriod,
-	        uint256 _ticketValidPeriod,
-	        uint256 _counter,
-	        bool _isAvailable,
-	        address _beneficiary
-	    )  external onlyOwner 
-
-	    {
-	        _editServiceTariff(
-	            _tariffIndex, 
-	            _timelockPeriod,
-	            _ticketValidPeriod,
-	            _counter,
-	            _isAvailable,
-	            _beneficiary
-	        );
-	    }
-
-	    function addTariffPayOption(
-	        uint256 _tariffIndex,
-	        address _paymentToken,
-	        uint256 _paymentAmount,
-	        uint16 _agentFeePercent
-	    ) external onlyOwner  returns(uint256)
-	    {
-	        return _addTariffPayOption(
-	            _tariffIndex,
-	            _paymentToken,
-	            _paymentAmount,
-	            _agentFeePercent
-	        );
-	    }
+	        ServiceProviderOwnable(_subscrRegistry) {}
 	    
-	    function editTariffPayOption(
-	        uint256 _tariffIndex,
-	        uint256 _payWithIndex, 
-	        address _paymentToken,
-	        uint256 _paymentAmount,
-	        uint16 _agentFeePercent
-	    ) external onlyOwner 
-	    {
-	        _editTariffPayOption(
-	            _tariffIndex,
-	            _payWithIndex, 
-	            _paymentToken,
-	            _paymentAmount,
-	            _agentFeePercent
-	        );
-	    } 
-
-	    function authorizeAgentForService(
-	        address _agent,
-	        uint256[] memory _serviceTariffIndexes
-	    ) external onlyOwner returns (uint256[] memory)
-	    {
-	        // TODO Check agent
-	        return _authorizeAgentForService(
-	            _agent,
-	            _serviceTariffIndexes
-	        );
-	    }
 
 	    ////////////////////////////
 	    //        Main USAGE      //
